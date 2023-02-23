bfa864efebdae22c2be0defcf3a7e410d80a7c0d
Zee Mudia <iosazee1@gmail.com>
2023-02-23 17:27:49 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import Manager from "./lib/Manager";
+import Engineer from "./lib/Engineer";
+import Intern from "./lib/Intern";
+import inquirer from "inquirer";
+import { resolve, join } from "path";
+import fs from "fs/promises";
+
+const OUTPUT_DIR = resolve(__dirname, "output");
+const outputPath = join(OUTPUT_DIR, "team.html");
+
+import render from "./src/page-template.js";
+
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
